3b6ff0f687e4177f9143ca12c3a59cb737370c2e
Elias <elias.retamales@metacontrol.cl>
2024-10-24 16:06:07 -0300

    Instalación de Jetstream para autenticación.

--- SRNexus/routes/web.php
+++ SRNexus/routes/web.php
@@ -24,3 +24,13 @@ Route::get('/write-influx', [InfluxTestController::class, 'writeData']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
