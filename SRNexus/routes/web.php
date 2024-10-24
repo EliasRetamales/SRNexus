3c53c84f59b6a9582595d1282b0ee39f8799865d
Elias <elias.retamales@metacontrol.cl>
2024-10-24 16:37:39 -0300

    Autenticación con verificación

--- SRNexus/routes/web.php
+++ SRNexus/routes/web.php
@@ -3,34 +3,45 @@
 use App\Http\Controllers\InfluxdbConnectionController;
 use App\Http\Controllers\InfluxTestController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// routes/web.php
-
-Route::resource('influxdb_connections', InfluxdbConnectionController::class);
-Route::get('/test-influx', [InfluxTestController::class, 'testConnection']);
-Route::get('/write-influx', [InfluxTestController::class, 'writeData']);
-
+// Ruta de inicio
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// Rutas de autenticación y verificación de correo electrónico
+Route::middleware('auth')->group(function () {
+    // Ruta para mostrar la notificación de verificación
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->name('verification.notice');
+
+    // Ruta para verificar el correo electrónico
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect('/dashboard');
+    })->middleware(['signed'])->name('verification.verify');
+
+    // Ruta para reenviar la notificación de verificación
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Se ha enviado un nuevo enlace de verificación a tu correo electrónico.');
+    })->middleware(['throttle:6,1'])->name('verification.send');
+
+    // Rutas protegidas que requieren autenticación y verificación de correo
+    Route::middleware(['verified'])->group(function () {
+        // Ruta al dashboard
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
+        // Rutas de recursos y otras rutas protegidas
+        Route::resource('influxdb_connections', InfluxdbConnectionController::class);
+        Route::get('/test-influx', [InfluxTestController::class, 'testConnection']);
+        Route::get('/write-influx', [InfluxTestController::class, 'writeData']);
+    });
 });
