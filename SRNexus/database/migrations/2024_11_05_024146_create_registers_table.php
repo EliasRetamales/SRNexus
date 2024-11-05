c43f59919dabbd53b229a50381368e632cf391d4
Elias <elias.retamales@metacontrol.cl>
2024-11-04 23:42:43 -0300

    S5_TP1_4 → Register Table Migration

---
+++ SRNexus/database/migrations/2024_11_05_024146_create_registers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->id();
+            $table->float('value');
+            $table->timestamp('measurement_time'); // Fecha de la medición
+            $table->boolean('enable')->default(true);
+            $table->timestamps(); // created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registers');
+    }
+}
